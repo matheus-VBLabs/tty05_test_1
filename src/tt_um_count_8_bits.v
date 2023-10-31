9f6f5c9a84e507cbf862ecdaf58af418ced93a46
matheus-VBLabs <147437930+matheus-VBLabs@users.noreply.github.com>
2023-10-31 17:21:42 -0300

    Update tt_um_count_8_bits.v

--- src/tt_um_count_8_bits.v
+++ src/tt_um_count_8_bits.v
@@ -41,15 +41,26 @@ endmodule
 // Careful: this file (hdl.v) will be automatically replaced when you ask
 // to generate code from BLOCKS buttons.
 module tt_um_count_8_bits  (
-    output wire [7:0] uo_out,   // Dedicated outputs 
+    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // will go high when the design is enabled
-    input  wire       clk       // clock
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
 );
 
 
   //input wire clk,
-wire A8, A7, A6, A5, A4, A3, A2, A1;
-
+  output wire A3,
+  output wire A4,
+  output wire A2,
+  output wire A5,
+  output wire A1,
+  output wire A6,
+  output wire A8,
+  output wire A7
 
 assign uo_out[7:0] = {A8, A7, A6, A5, A4, A3, A2, A1};
 
